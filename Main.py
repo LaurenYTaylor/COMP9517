9838feaff3bf4407d746c6f28202d6fa8d1f665d
Huntfalow123 <bsmith4680@gmail.com>
2019-07-17 09:02:38 +1000

    Data_Aug//Padding//Equalisation

--- Main.py
+++ Main.py
@@ -29,23 +29,69 @@ labelsO = [int(i.split('-labels')[1][0:2]) for i in labels]
 fileI = [i.split('\\')[1] for i in file_name]
 fileO = [i.split('\\')[1] for i in labels]
 
-Input = {'filename':fileI,'label':labelsI}
-Output = {'filename':fileO,'label':labelsO}
+Input = {'filename':fileI,'label':labelsI,'filedir':file_name}
+Output = {'filename':fileO,'label':labelsO,'filedir':labels}
 
 Input = pd.DataFrame(data=Input)
 Output = pd.DataFrame(data=Output)
 
+def equalize(img):
+    """Takes and image and returns the same image with the histogram equalized"""
+    return cv.equalizeHist(img)
 
-
-def DataRot(image,rotation = 0):
+def dataRot(image,rotation = 0):
     hieght, width = image.shape
     M = cv.getRotationMatrix2D((width/2,hieght/2),rotation,1)
     out = cv.warpAffine(image,M,(width,hieght))
     return out
 
-def DataFlip(image, flip = 0):
+def dataFlip(image, flip = 0):
     out = cv.flip(image, flip)
     return out
+
+def mirrorPadding(image,padding=0):
+    out = cv.copyMakeBorder(image, padding, padding, padding, padding, cv.BORDER_REFLECT_101, None)
+    return out
+
+def resnet_block(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu'):
+    """2D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+    x = inputs
+    x = conv(x)
+    x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+
+    return x
+
+if __name__ == '__main__':
+    img = cv.imread(Input.iloc[0,2],0)
+    new_img = mirrorPadding(img,1)
+    new_img = equalize(img)
+
+# use BORDER_REFLECT_101
 
 
 
